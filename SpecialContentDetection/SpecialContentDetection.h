d2f929a4266135ba8ae8ead8b795e7001ac8671d
Tamer Milad <tamer.milad6@gmail.com>
2018-03-18 09:41:12 +0800

    Add detection framework

---
+++ SpecialContentDetection/SpecialContentDetection.h
@@ -0,0 +1,19 @@
+//
+//  SpecialContentDetection.h
+//  SpecialContentDetection
+//
+//  Created by Tamer Pateer on 18/3/18.
+//  Copyright © 2018 Atlassian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpecialContentDetection.
+FOUNDATION_EXPORT double SpecialContentDetectionVersionNumber;
+
+//! Project version string for SpecialContentDetection.
+FOUNDATION_EXPORT const unsigned char SpecialContentDetectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpecialContentDetection/PublicHeader.h>
+
+
